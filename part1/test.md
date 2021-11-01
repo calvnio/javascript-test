fb0888ad8a27b4a41af5e6a6fd647d72e25c11fd
Calvin Tran <trancalvin@gmail.com>
2021-11-01 14:59:01 +1100

    Completed tasks in part1 and part2 for savee

--- part1/test.md
+++ part1/test.md
@@ -1,23 +1,38 @@
 ## React interview questions
 
 1. What is state and props in reactjs?
+   States are attributes of a component which store data which the page requires to run its functionalities.
+   Props are states or data passed down to a child component.
 
 2. What is high order component? How do you use it?
 
 3. What is context? What are the benefits of it?
+   Context is a method to pass data through components wihtout the need to pass props to child components. They can be interpreted similarly to global variables where React components can access wherever.
+   The benefits of this method is that it eliminates the need to pass props to child components, elinimating redundancy.
 
 4. How to assign and change the value of state in a component?
+   Assigning states can be done through constructors within the class components and modifying them using a method called "setState".
+   We can also use React hooks to assign and change values of states --> useState.
 
 5. Could you explain the life-cycle of a react component?
+   Life-cycle methods are used to free up resources taken when components are destroyed.
+   e.g. componentDidMount() and componentWillUnmount()
 
 6. What is fragment in react?
+   Fragment allows the ability to return multiple elements without adding extra nodes to the DOM.
 
 7. What is ref in react?
+   Ref is used to indicate html tags within the render-side of the component. They can be used as access keys to point to the component of the html.
 
 8. What is container component? What is presentational component?
 
 9. How to pass a function to a component?
+   html tags possess methods such as onclick which can be used to trigger a function. Functions can be declared outside the component itself and binded to the component to later then be called via the render tags of the component.
 
 10. What is portal?
+    A method to insert a child into a different location in the DOM.
 
 11. How to share state between parent component and child component? How to share state between sibling components?
+    States or data can be shared using props where the parent would pass the states as props to a child component.
+    Callback functions can be passed also to manipulate the states from the parent component.
+    Between siblings, we can do the same method however, this may be redundant use of code so we can install tools to help us control the states such as Redux.
--- part2/practice.js
+++ part2/practice.js
@@ -1,98 +1,126 @@
-function dummy () {
+function dummy() {
   return 1;
 }
 /**
  * Reverse an array
- * 
+ *
  * @example
- * 
+ *
  * reverse([1,2,3]) returns [3,2,1]
- * 
+ *
  * @param {array} array
  * @returns {array}
  */
-function reverse (array) {
-  return [];
+function reverse(array) {
+  return array.reverse();
 }
 
 /**
  * Reverse a string
- * 
+ *
  * @example
- * 
+ *
  * stringReverse('hello') returns 'olleh'
- * 
+ *
  * @param {string} text
  * @returns {string}
- * 
+ *
  */
-function stringReverse (text) {
-  return '';
+function stringReverse(text) {
+  return text.split("").reverse().join("");
 }
-
 
 /**
  * return a fixed length fibonacci list
- * 
+ *
  * @example
- * 
+ *
  * fibonacci(5) returns [1,1,2,3,5]
- * 
+ *
  * @param {number} length
  * @returns {array}
  */
-function fibonacci (length) {
-  return [];
+function getFibonacciNum(index) {
+  if (index < 2) return 1;
+  return getFibonacciNum(index - 2) + getFibonacciNum(index - 1);
+}
+function fibonacci(length) {
+  let arr = [];
+  for (let i = 0; i < length; i++) {
+    arr.push(getFibonacciNum(i));
+  }
+  return arr;
 }
 
 /**
  * return the biggest number in the array
- * 
+ *
  * @example
- * 
+ *
  * biggest([1,3,5,7,9]) returns 9
- * 
+ *
  * @param {array} array
  * @returns {number}
  */
-function biggest (array) {
-  return 0;
+function biggest(array) {
+  return Math.max(...array);
 }
 
 /**
- * Creates an array of numbers progressing from 
+ * Creates an array of numbers progressing from
  * start up to, but not including, end
- * 
+ *
  * YOU HAVE TO IMPLEMENT YOUR OWN ALGORITHM HERE!!!
  * DON'T USE ANY 3RD PARTY LIBRARIES.
- * 
+ *
  * @example
- * 
+ *
  * range(0,4) returns [0,1,2,3]
- * 
- * @param {number} start 
- * @param {number} end 
+ *
+ * @param {number} start
+ * @param {number} end
  * @returns {array}
  */
-function range (start, end) {
-  return [];
+function range(start, end) {
+  let arr = [];
+  for (let i = start; i < end; i++) {
+    arr.push(i);
+  }
+  return arr;
 }
 
 /**
  * Recursively flattens array.
- * 
+ *
  * YOU HAVE TO IMPLEMENT YOUR OWN ALGORITHM HERE!!!
  * DON'T USE ANY 3RD PARTY LIBRARIES.
- * 
+ *
  * @example
- * 
+ *
  * flatten([1,[2,[3,4],[5,[6]]]]) returns [1,2,3,4,5,6]
- * 
+ *
  * @param {array} array
  * @returns {array}
  */
-function flatten (array) {
-  return [];
+function deconstruct(array, flattenArr) {
+  if (Array.isArray(array)) {
+    for (let i = 0; i < array.length; i++) {
+      if (Array.isArray(array[i])) {
+        deconstruct(array[i], flattenArr);
+      } else {
+        flattenArr.push(array[i]);
+      }
+    }
+  } else {
+    flattenArr.push(array);
+  }
+}
+function flatten(array) {
+  let flattenArr = [];
+  for (let i = 0; i < array.length; i++) {
+    deconstruct(array[i], flattenArr);
+  }
+  return flattenArr;
 }
 
 module.exports = {
@@ -103,5 +131,5 @@ module.exports = {
   biggest,
   flatten,
   stringReverse,
-  range
+  range,
 };
